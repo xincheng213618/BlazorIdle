377b5bb812fe13c2ced328f76b9bc2a0fd09288c
Solaireshen97 <solairesjh@outlook.com>
2025-10-28 17:43:34 +0800

    新增 GameConfigResponse 类及默认配置方法
    添加了一个新的密封类 `GameConfigResponse`，用于存储游戏配置数据，包括版本信息、职业列表和怪物列表。
    在 `DefaultGameConfig` 类中新增了两个静态方法 `DefaultProfessions` 和 `DefaultMonsters`，分别提供默认职业 "Warrior" 和默认怪物 "Green Slime" 的定义。

---
+++ BlazorIdle.Shared/DTOs/GameConfigResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using BlazorIdle.Game.Config;
+
+namespace BlazorIdle.Shared.DTOs
+{
+    public sealed class GameConfigResponse
+    {
+        public string Version { get; set; } = "unloaded";
+        public List<ProfessionDef> Professions { get; set; } = new();
+        public List<MonsterDef> Monsters { get; set; } = new();
+    }
+}
---
+++ BlazorIdle.Shared/Game/Config/DefaultGameConfig.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace BlazorIdle.Game.Config
+{
+    public static class DefaultGameConfig
+    {
+        public static List<ProfessionDef> DefaultProfessions() => new()
+        {
+            new ProfessionDef
+            {
+                Id = "warrior", Name = "Warrior", Desc="Fallback",
+                MaxHp = 280, AttackRateAPS = 1.8, DamagePerAttack = 18,
+                HastePercent = 0, SpecialIntervalSec = 6, SpecialDamage = 140,
+                CritChancePercent = 10, CritMultiplier = 1.5, VariancePct = 0.04,
+                ReviveSec = 5.0
+            }
+        };
+
+        public static List<MonsterDef> DefaultMonsters() => new()
+        {
+            new MonsterDef
+            {
+                Id = "slime", Name = "Green Slime", Desc="Fallback", Level=1,
+                MaxHp = 220, AttackIntervalSec = 2.0, DamagePerHit = 8,
+                VariancePct = 0.05, RespawnSec = 2.5
+            }
+        };
+    }
+}
